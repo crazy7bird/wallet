06ca9bb133f999f066e8d41270d6cf09b642b7a9
crazybird <roy7alexandre@gmail.com>
2024-01-05 15:06:07 +0100

    [WIP] - working on str to timestamp function

--- add.c
+++ add.c
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <time.h>
 #include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 /**
 * Contain options from args
 */
@@ -18,8 +21,45 @@ struct option{
 }option_default = {0,false,0,false,0,false,0,false};
 typedef struct option option;
 
+
+#define T_MAX_CHAR 17u
+#define T_MIN_CHAR 8u // DD/MM/YY or 
+#define MIN_DATE 1231110000 // 1er Jan 2009, you probably have no crypto back then.
+/**
+ * @brief Convert a string to a timestamp.
+ *  The string could be DD/MM/YY-HH:MM:SS format or a real timestamp (64 bit number) but in str.
+ * 
+ * @param s the string with time information.
+ * @return time_t timestamp decoded.
+ */
 static time_t timeformat(char * s){
-    printf("timeformat entry : %s \n", s );
+    bool b_err = false;
+    size_t s_size = strlen(s);
+    time_t t;
+    printf("timeformat entry : %s \n", s );
+    // Need to separate real timestamp and date formated DD/MM/YY-HH:MM:SS
+    if(s_size > T_MAX_CHAR || s_size < T_MIN_CHAR){
+      b_err = true;
+    }
+    // Determine if it’s a Timestamp or a string.
+    bool s_is_just_a_bunch_of_digits = true;
+    for(int i=0; i<s_size && s_is_just_a_bunch_of_digits; i++){
+      s_is_just_a_bunch_of_digits = isdigit(s[i]);
+    }
+    if(s_is_just_a_bunch_of_digits){
+      t = atoll(s);
+    }
+    else{
+      struct tm tm;
+      // @todo : have to creat strptime…
+      if ( strptime(s, "%Y/%m/%d-%H:%M:%S", &tm) != NULL ) t = mktime(&tm);
+    }
+    if(b_err){
+      printf("ERROR in time format (-d) please use :\n  - Timestamp number\n  - dateformat: DD/MM/YY-HH:MM:SS\nNote: dateformat could stop at years\n");
+      return 0;
+    }
+    printf("time - %ld \n",t);
+    return t;
 }
 
 static option  options_manager(int argc, char** argv){
